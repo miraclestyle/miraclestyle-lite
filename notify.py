ce604257c50c4770c51e2bfd21b46bc7c26e6c19
miraclestyle <miraclestyle@devtable.com>
2013-07-23 22:19:18 +0000

    adding notify engine basics

---
+++ notify.py
@@ -0,0 +1,23 @@
+#coding=UTF-8
+
+import webapp2
+from google.appengine.api import taskqueue
+from google.appengine.api import mail
+from google.appengine.ext import ndb
+
+# ovo je neki predlog osnovice, tu se treba ubaciti jos inteligencije, handlovanje gresaka, akrobacije....
+class NotifyEngine(webapp2.RequestHandler):
+    def post(self):
+        object_log_key = self.request.get('object_log_key')
+        notify_sender = self.request.get('notify_sender')
+        notify_to = self.request.get('notify_to')
+        notify_subject = self.request.get('notify_subject')
+        def notify():
+            object_log = ObjectLog.get_by_id(object_log_key)
+            mail.send_mail(sender=notify_sender, to=notify_to, subject=notify_subject, body=object_log.message)
+        db.run_in_transaction(notify)
+
+
+app = webapp2.WSGIApplication([
+                              ('/notify', NotifyEngine)],
+                              debug=True)
