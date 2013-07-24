c2824071c8e1b0856f72942b3116857f9bb64538
miraclestyle <miraclestyle@devtable.com>
2013-07-24 15:35:24 +0000

    notify improvement

--- notify.py
+++ notify.py
@@ -8,14 +8,16 @@ from google.appengine.ext import ndb
 # ovo je neki predlog osnovice, tu se treba ubaciti jos inteligencije, handlovanje gresaka, akrobacije....
 class NotifyEngine(webapp2.RequestHandler):
     def post(self):
+        reference_key = self.request.get('reference_key')
         object_log_key = self.request.get('object_log_key')
         notify_sender = self.request.get('notify_sender')
         notify_to = self.request.get('notify_to')
         notify_subject = self.request.get('notify_subject')
         notify_outlet = self.request.get('notify_outlet')
         def notify():
-          if (notify_outlet == 'email'):
-            object_log = ObjectLog.get_by_id(object_log_key)
+          object_log_future = ObjectLog.get_by_id_async(object_log_key, parent=reference_key)
+          if (notify_outlet == 'email' | notify_outlet == None):
+            object_log = object_log_future.get_result()
             mail.send_mail(sender=notify_sender, to=notify_to, subject=notify_subject, body=object_log.message)
         db.run_in_transaction(notify)
 
