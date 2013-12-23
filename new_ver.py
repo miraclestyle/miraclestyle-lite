9077380789ee8f98d1ef6da13bf3a7dfce019df9
miraclestyle <elvinkosova@gmail.com>
2013-12-23 12:49:09 +0000

    updates

--- new_ver.py
+++ new_ver.py
@@ -10,40 +10,5 @@ class ActionPermission(Permission):
     
   def run(self, role, context):
     
-    if (self.kind == context.entity.get_rule_kind()) and (self.action in context.entity._rule_actions) and (eval(self.condition)):
-      if (role.overide):
-        if (self.executable != None):
-          context.entity._rule_action_permissions[self.action] = {'executable': self.executable}
-      else:
-        if (self.executable != None) and (context.entity._rule_action_permissions[self.action]['executable'] == None):
-          context.entity._rule_action_permissions[self.action] = {'executable': self.executable}
-
-class FieldPermission(Permission):
-  
-  
-  def __init__(self, kind, field, writable=None, visible=None, required=None, condition=None):
-    
-    self.kind = kind
-    self.field = field
-    self.writable = writable
-    self.visible = visible
-    self.required = required
-    self.condition = condition
-    
-  def run(self, context):
-    
-    if (self.kind == context.entity.get_rule_kind()) and (self.field in context.entity._rule_properties) and (eval(self.condition)):
-      if (role.overide):
-        if (self.writable != None):
-          context.entity._rule_field_permissions[self.field] = {'writable': self.writable}
-        if (self.visible != None):
-          context.entity._rule_field_permissions[self.field] = {'visible': self.visible}
-        if (self.required != None):
-          context.entity._rule_field_permissions[self.field] = {'required': self.required}
-      else:
-        if (self.writable != None) and (context.entity._rule_field_permissions[self.field]['writable'] == None):
-          context.entity._rule_field_permissions[self.field] = {'writable': self.writable}
-        if (self.visible != None) and (context.entity._rule_field_permissions[self.field]['visible'] == None):
-          context.entity._rule_field_permissions[self.field] = {'visible': self.visible}
-        if (self.required != None) and (context.entity._rule_field_permissions[self.field]['required'] == None):
-          context.entity._rule_field_permissions[self.field] = {'required': self.required}
+    if (self.kind == context.entity.get_rule_kind()) and (self.action in context.entity._rule_actions) and (eval(self.condition)) and (self.executable != None):
+      role.action_permissions[self.action] = {'executable': [].append(self.executable)}
