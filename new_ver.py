bbf1e17cacd17c546cb8fa6cc6674d753bc5c2ae
miraclestyle <elvinkosova@gmail.com>
2013-12-23 12:12:27 +0000

    updates

--- new_ver.py
+++ new_ver.py
@@ -1,3 +1,23 @@
+class ActionPermission(Permission):
+  
+  
+  def __init__(self, kind, action, executable=None, condition=None):
+    
+    self.kind = kind
+    self.action = action
+    self.executable = executable
+    self.condition = condition
+    
+  def run(self, role, context):
+    
+    if (self.kind == context.entity.get_rule_kind()) and (self.action in context.entity._rule_actions) and (eval(self.condition)):
+      if (role.overide):
+        if (self.executable != None):
+          context.entity._rule_action_permissions[self.action] = {'executable': self.executable}
+      else:
+        if (self.executable != None) and (context.entity._rule_action_permissions[self.action]['executable'] == None):
+          context.entity._rule_action_permissions[self.action] = {'executable': self.executable}
+
 class FieldPermission(Permission):
   
   
@@ -12,18 +32,18 @@ class FieldPermission(Permission):
     
   def run(self, context):
     
-    if (self.kind == context.entity._get_kind()) and (self.field in context.entity._properties) and (eval(self.condition)):
-      if (context.overide):
+    if (self.kind == context.entity.get_rule_kind()) and (self.field in context.entity._rule_properties) and (eval(self.condition)):
+      if (role.overide):
         if (self.writable != None):
-          context.entity._field_permissions[self.field] = {'writable': self.writable}
+          context.entity._rule_field_permissions[self.field] = {'writable': self.writable}
         if (self.visible != None):
-          context.entity._field_permissions[self.field] = {'visible': self.visible}
+          context.entity._rule_field_permissions[self.field] = {'visible': self.visible}
         if (self.required != None):
-          context.entity._field_permissions[self.field] = {'required': self.required}
+          context.entity._rule_field_permissions[self.field] = {'required': self.required}
       else:
-        if (context.entity._field_permissions[self.field]['writable'] == None) and (self.writable != None):
-          context.entity._field_permissions[self.field] = {'writable': self.writable}
-        if (context.entity._field_permissions[self.field]['visible'] == None) and (self.visible != None):
-          context.entity._field_permissions[self.field] = {'visible': self.visible}
-        if (context.entity._field_permissions[self.field]['required'] == None) and (self.required != None):
-          context.entity._field_permissions[self.field] = {'required': self.required}
+        if (self.writable != None) and (context.entity._rule_field_permissions[self.field]['writable'] == None):
+          context.entity._rule_field_permissions[self.field] = {'writable': self.writable}
+        if (self.visible != None) and (context.entity._rule_field_permissions[self.field]['visible'] == None):
+          context.entity._rule_field_permissions[self.field] = {'visible': self.visible}
+        if (self.required != None) and (context.entity._rule_field_permissions[self.field]['required'] == None):
+          context.entity._rule_field_permissions[self.field] = {'required': self.required}
