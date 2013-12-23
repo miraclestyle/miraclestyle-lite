6bcc064d24b80bc507b6f6630239869e07f44707
miraclestyle <elvinkosova@gmail.com>
2013-12-23 13:11:31 +0000

    new_ver.py edited online with Bitbucket

--- new_ver.py
+++ new_ver.py
@@ -1,3 +1,26 @@
+class Engine:
+  
+  @classmethod
+  def final_check(cls, context):
+    pass
+  
+  @classmethod
+  def run(cls, context):
+    
+    # prvo se radi procesing local_role
+    # pa se onda apply strict
+    # i onda se procesira global_role
+    # pase apply overide
+    
+    entity = context.entity
+    if hasattr(entity, '_global_role') and isinstance(entity._global_role, Role):
+       entity._global_role.run(context)
+    
+    cls.final_check(context) # ova funkcija proverava sva polja koja imaju vrednosti None i pretvara ih u False
+
+
+
+
 class ActionPermission(Permission):
   
   
@@ -11,4 +34,9 @@ class ActionPermission(Permission):
   def run(self, role, context):
     
     if (self.kind == context.entity.get_rule_kind()) and (self.action in context.entity._rule_actions) and (eval(self.condition)) and (self.executable != None):
-      role.action_permissions[self.action] = {'executable': [].append(self.executable)}
+      if (role.overide):
+        if (self.executable != None):
+          context.entity._rule_action_permissions[self.action]['executable'] = [self.executable]
+      else:
+        if (self.executable != None):
+          context.entity._rule_action_permissions[self.action]['executable'].append(self.executable)
