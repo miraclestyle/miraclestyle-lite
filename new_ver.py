10c863fb6443099199edff1f3d82b3c53f279ef4
miraclestyle <elvinkosova@gmail.com>
2013-12-24 12:37:51 +0000

    new_ver.py edited online with Bitbucket

--- new_ver.py
+++ new_ver.py
@@ -1,22 +1,25 @@
-class Engine:
+class CartInit:
   
-  @classmethod
-  def final_check(cls, context):
-    pass
-  
-  @classmethod
-  def run(cls, context):
-    
-    calc = {}
-    
-    for role in roles:
-      role.run(context)
-    for action, properties in context.entity._rule_action_permissions.items():
-      for proerpty, value in properties.items():
-        if len(value):
-          if (strict) and all(value):
-            calc[action][proerpty] = [True]
-          elif any(value):
-            calc[action][proerpty] = [True]
-          else:
-            calc[action][proerpty] = [False]
+  def run(user_key, catalog_key):
+    # ucitaj postojeci entry na kojem ce se raditi write
+    catalog = catalog_key.get()
+    company = catalog.company.get()
+    entry = Entry.query(Entry.journal == ndb.Key('Journal', 'order'), 
+                        Entry.company == company, Entry.state.IN(['cart', 'checkout', 'processing']),
+                        Entry.party == user_key
+                        ).get()
+    # ako entry ne postoji onda ne pravimo novi entry na kojem ce se raditi write
+    if not (entry):
+      entry = Entry()
+      entry.journal = ndb.Key('Journal', 'order')
+      entry.company = company
+      entry.state = 'cart'
+      entry.date = datetime.datetime.today()
+      entry.party = user_key
+    # proveravamo da li je entry u state-u 'cart'
+    if (entry.state != 'cart'):
+      # ukoliko je entry u drugom state-u od 'cart' satate-a, onda abortirati pravljenje entry-ja
+      # taj abortus bi trebala da verovatno da bude neka "error" class-a koju client moze da interpretira useru
+      return 'ABORT'
+    else:
+      return entry
