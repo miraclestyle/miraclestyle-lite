c396e9a44233701d9cb8523207bd6dcfce0114b2
admin <vertazzar@gmail.com>
2014-02-03 12:48:46 +0100

    update

--- static/angular/app/js/controllers.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/* Here we define kernel controllers and deploy the AppControllers module */
-
-var AppControllers = angular.module('App.controllers', [])
-    .controller('Boot', ['$scope', function($scope) {
-		$scope.user = crUser;
-		log('Boot');
-}]);
