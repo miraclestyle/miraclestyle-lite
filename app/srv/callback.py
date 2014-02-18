f318eb9ab87dff78dd9e3d63bf2b259afa4feb3c
miraclestyle <elvinkosova@gmail.com>
2014-02-18 12:30:57 +0000

    callback.py edited online with Bitbucket

--- app/srv/callback.py
+++ app/srv/callback.py
@@ -16,7 +16,6 @@ class Engine:
       if context.callback.transactional is None:
         context.callback.transactional = ndb.in_transaction()
       if context.callback.transactional:
-        if len(context.callback.inputs) > 5:
-          context.callback.inputs = context.callback.inputs[:5]
+        context.callback.inputs = context.callback.inputs[:5]
       for input in context.callback.inputs:
         taskqueue.add(queue_name='io', url='/task/io_engine_run', params=input, transactional=context.callback.transactional)
