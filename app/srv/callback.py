6a1b2cd097d2fe88241f5d5dd55a1460eba8eadc
miraclestyle <elvinkosova@gmail.com>
2014-02-18 12:16:24 +0000

    transaction fix

--- app/srv/callback.py
+++ app/srv/callback.py
@@ -5,6 +5,7 @@ class Context():
   
   def __init__(self):
     self.inputs = []
+    self.transactional = False
 
 
 class Engine:
@@ -12,5 +13,8 @@ class Engine:
   @classmethod
   def run(cls, context):
     if len(context.callback.inputs):
+      if context.callback.transactional:
+        if len(context.callback.inputs) > 5:
+          context.callback.inputs = context.callback.inputs[:5]
       for input in context.callback.inputs:
-        taskqueue.add(queue_name='io', url='/task/io_engine_run', params=input, transactional=True)
+        taskqueue.add(queue_name='io', url='/task/io_engine_run', params=input, transactional=context.callback.transactional)
