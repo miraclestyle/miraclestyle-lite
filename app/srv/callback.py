cd70a45cb102be4e59942991be0040eb2665d144
miraclestyle <elvinkosova@gmail.com>
2014-02-24 17:33:53 +0000

    callback.py revision!

--- app/srv/callback.py
+++ app/srv/callback.py
@@ -1,5 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Feb 20, 2014
+
+@authors:  Edis Sehalic (edis.sehalic@gmail.com), Elvin Kosova (elvinkosova@gmail.com)
+'''
+
 from google.appengine.api import taskqueue
+
 from app import ndb
+
 
 class Context():
   
@@ -13,15 +22,12 @@ class Engine:
   @classmethod
   def run(cls, context):
     if len(context.callback.inputs):
+      context.callback.inputs = context.callback.inputs[:5]
       if context.callback.transactional is None:
         context.callback.transactional = ndb.in_transaction()
- 
-      context.callback.inputs = context.callback.inputs[:5]  
-        
-      queue = taskqueue.Queue(name='io')
+      queue = taskqueue.Queue(name='callback')
       tasks = []
       for input in context.callback.inputs:
-          tasks.append(taskqueue.Task(url='/task/io_engine_run', params=input))
-      
+        tasks.append(taskqueue.Task(url='/task/io_engine_run', params=input))
       if tasks:
         queue.add(tasks, transactional=context.callback.transactional)
