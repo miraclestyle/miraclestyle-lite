1c86d7f9141461ae536977b99b1b4b317daf3813
miraclestyle <elvinkosova@gmail.com>
2014-04-07 12:27:11 +0000

    callback.py revision! Testing required!

--- app/srv/callback.py
+++ app/srv/callback.py
@@ -4,6 +4,7 @@ Created on Feb 20, 2014
 
 @authors:  Edis Sehalic (edis.sehalic@gmail.com), Elvin Kosova (elvinkosova@gmail.com)
 '''
+
 import json
 
 from google.appengine.api import taskqueue
@@ -22,16 +23,12 @@ class Engine:
   
   @classmethod
   def run(cls, context):
- 
     tasks = {}
     url = '/task/io_engine_run'
-    
     if context.callback.transactional is None:
       context.callback.transactional = ndb.in_transaction()
-      
     if context.callback.transactional:
       context.callback.payloads = context.callback.payloads[:5]
- 
     if len(context.callback.payloads):
       for payload in context.callback.payloads:
         queue_name, data = payload
@@ -39,15 +36,11 @@ class Engine:
           kwargs = payload[2]
         except IndexError as e:
           kwargs = {}
-          
         if queue_name not in tasks:
           tasks[queue_name] = []
-          
         tasks[queue_name].append(taskqueue.Task(url=url, payload=json.dumps(data), **kwargs))
-        
     if len(tasks):
       for queue_name, tasks in tasks.items():
-         queue = taskqueue.Queue(name=queue_name)
-         queue.add(tasks, transactional=context.callback.transactional)
- 
+        queue = taskqueue.Queue(name=queue_name)
+        queue.add(tasks, transactional=context.callback.transactional)
     context.callback.payloads = []
