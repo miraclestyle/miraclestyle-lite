20f688490d2a0f4a51b43829b72997c6983d34af
miraclestyle <elvinkosova@gmail.com>
2014-04-14 09:30:14 +0000

    ndb.py revision!

--- app/ndb.py
+++ app/ndb.py
@@ -590,8 +590,7 @@ class SuperDateTimeProperty(_BaseProperty, DateTimeProperty):
       single = True
     out = []
     for val in value:
-       out.append(datetime.datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%fZ'))
-    
+      out.append(datetime.datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%fZ'))
     if single:
       try:
         return out[0]
