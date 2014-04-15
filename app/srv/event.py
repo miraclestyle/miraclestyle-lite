cfb3db2c0f063ecf8e69d672f24ed2a3afcccd9a
miraclestyle <elvinkosova@gmail.com>
2014-04-15 11:24:35 +0000

    event.py revision! Testing required!

--- app/srv/event.py
+++ app/srv/event.py
@@ -8,19 +8,6 @@ Created on Jan 6, 2014
 from app import ndb
 
 
-__SYSTEM_ACTIONS = {}
-
-def get_system_action(action_key):
-  global __SYSTEM_ACTIONS
-  action_key = ndb.Key(Action, action_key)
-  return __SYSTEM_ACTIONS.get(action_key.urlsafe())
-
-def register_system_action(*actions):
-  global __SYSTEM_ACTIONS
-  for action in actions:
-    __SYSTEM_ACTIONS[action.key.urlsafe()] = action
-
-
 class Action(ndb.BaseExpando):
   
   _kind = 56
@@ -28,13 +15,12 @@ class Action(ndb.BaseExpando):
   name = ndb.SuperStringProperty('1', required=True)
   arguments = ndb.SuperPickleProperty('2', default={})
   active = ndb.SuperBooleanProperty('3', default=True)
-  service = ndb.SuperStringProperty('4')  # Service name, such as log, notify, transaction, etc. @todo Probably to be removed!
-  operation = ndb.SuperStringProperty('5')  # Operation name, that is being called inside a service, such as, read, write, etc.
+
+
+class Plugin(ndb.BasePolyExpando):
   
-  @classmethod
-  def get_local_action(cls, action_key):
-    action = ndb.Key(urlsafe=action_key).get()
-    if action.active:
-      return action
-    else:
-      return None
+  _kind = 52
+  
+  sequence = ndb.SuperIntegerProperty('1', required=True)
+  subscriptions = ndb.SuperKeyProperty('2', kind='56', repeated=True)
+  active = ndb.SuperBooleanProperty('3', required=True, default=True)
