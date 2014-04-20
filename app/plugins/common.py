08add466dd543a9e0e92e1f1d5da6776ee78d6c8
miraclestyle <elvinkosova@gmail.com>
2014-04-20 15:16:37 +0000

    common.py edited online with Bitbucket

--- app/plugins/common.py
+++ app/plugins/common.py
@@ -12,24 +12,47 @@ from app.srv import event, callback
 from.app.plugins import log, callback
 
 
+def select_entities(context, entities):
+  entities = []
+  if len(entities):
+    for kind_id in entities:
+      if kind_id in context.entities:
+        entities.append(context.entities[kind_id])
+  else:
+    entities.append(context.entities[context.model.get_kind()])
+  return entities
+
+
 class Prepare(event.Plugin):
   
-  domain_model = ndb.SuperBooleanProperty('4', required=True, indexed=False, default=True)
+  kind_id = ndb.SuperStringProperty('4', required=False, indexed=False)
+  domain_model = ndb.SuperBooleanProperty('5', required=True, indexed=False, default=True)
   
   def run(self, context):
     if not context.entities:
       context.entities = {}
-    if self.domain_model:
-      context.entities[context.model.get_kind()] = context.model(namespace=context.domain.key_namespace)
+    if self.kind_id != None:
+      if self.domain_model:
+        context.entities[self.kind_id] = context.model(namespace=context.domain.key_namespace)
+      else:
+        context.entities[self.kind_id] = context.model()
     else:
-      context.entities[context.model.get_kind()] = context.model()
+      if self.domain_model:
+        context.entities[context.model.get_kind()] = context.model(namespace=context.domain.key_namespace)
+      else:
+        context.entities[context.model.get_kind()] = context.model()
 
 
 class Read(event.Plugin):
   
+  kind_id = ndb.SuperStringProperty('4', required=False, indexed=False)
+  
   def run(self, context):
     entity_key = context.input.get('key')
-    context.entities[context.model.get_kind()] = entity_key.get()
+    if self.kind_id != None:
+      context.entities[self.kind_id] = entity_key.get()
+    else:
+      context.entities[context.model.get_kind()] = entity_key.get()
 
 
 class Write(event.Plugin):
@@ -38,14 +61,7 @@ class Write(event.Plugin):
   
   @ndb.transactional(xg=True)
   def run(self, context):
-    entities = []
-    if len(self.write_entities):
-      for kind_id in self.write_entities:
-        if kind_id in context.entities:
-          entities.append(context.entities[kind_id])
-    else:
-      for kind_id, entity in context.entities.items():
-        entities.append(entity)
+    entities = select_entities(context, self.write_entities)
     ndb.put_multi(entities)
     log.write(context)
     callback.execute(context)
@@ -57,14 +73,7 @@ class Delete(event.Plugin):
   
   @ndb.transactional(xg=True)
   def run(self, context):
-    entities = []
-    if len(self.delete_entities):
-      for kind_id in self.delete_entities:
-        if kind_id in context.entities:
-          entities.append(context.entities[kind_id])
-    else:
-      for kind_id, entity in context.entities.items():
-        entities.append(entity)
+    entities = select_entities(context, self.delete_entities)
     ndb.delete_multi(entities)
     log.write(context)
     callback.execute(context)
@@ -72,8 +81,13 @@ class Delete(event.Plugin):
 
 class Output(event.Plugin):
   
+  kind_id = ndb.SuperStringProperty('4', required=False, indexed=False)
+  
   def run(self, context):
-    context.output['entity'] = context.entities[context.model.get_kind()]
+    if self.kind_id != None:
+      context.output['entity'] = context.entities[self.kind_id]
+    else:
+      context.output['entity'] = context.entities[context.model.get_kind()]
 
 
 class Field(ndb.BaseModel):
@@ -84,22 +98,34 @@ class Field(ndb.BaseModel):
 
 class FieldAutoUpdate(event.Plugin):
   
-  fields = ndb.SuperLocalStructuredProperty(Field, '4', repeated=True, indexed=False)
+  kind_id = ndb.SuperStringProperty('4', required=False, indexed=False)
+  fields = ndb.SuperLocalStructuredProperty(Field, '5', repeated=True, indexed=False)
   
   def run(self, context):
-    for field in self.fields:
-      setattr(context.values, field.name, field.value)
+    if self.kind_id != None:
+      for field in self.fields:
+        setattr(context.values[self.kind_id], field.name, field.value)
+    else:
+      for field in self.fields:
+        setattr(context.values[context.model.get_kind()], field.name, field.value)
 
 
 class Search(event.Plugin):
   
-  search_filter_callback = ndb.SuperStringProperty('4', required=True, indexed=False)
-  search_entities_callback = ndb.SuperStringProperty('5', required=True, indexed=False)
+  kind_id = ndb.SuperStringProperty('4', required=False, indexed=False)
+  search = ndb.SuperSearchProperty('5', required=False, indexed=False)  # @todo Transform this field to include optional query parameters to include in query.
   
   def run(self, context):
-    if context.entity.key:
-      namespace = context.entity.key_namespace
-    query = context.model.query(namespace=namespace)  # @todo not sure if we should extract model from entity (model = context.entity.__class__)?
+    namespace = None
+    if self.kind_id != None:
+      model = context.entities[self.kind_id].__class__
+      if context.entities[self.kind_id].key:
+        namespace = context.entities[self.kind_id].key_namespace
+    else:
+      model = context.model
+      if context.entities[context.model.get_kind()].key:
+        namespace = context.entities[context.model.get_kind()].key_namespace
+    query = model.query(namespace=namespace)
     search = context.input.get('search')
     if search:
       filters = search.get('filters')
@@ -109,7 +135,7 @@ class Search(event.Plugin):
         field = getattr(model, _filter['field'])
         op = _filter['operator']
         value = _filter['value']
-        if op == '==': # here we need more ifs for >=, <=, <, >, !=, IN ... OR ... ? this algo needs improvements
+        if op == '==': # here we need more ifs for >=, <=, <, >, !=, IN ... OR ... ? this also needs improvements
           args.append(field == value)
         elif op == '!=':
           args.append(field != value)
@@ -122,25 +148,16 @@ class Search(event.Plugin):
         elif op == 'IN':
           args.append(field.IN(value))
       query = query.filter(*args)  
-      order_by_field = getattr(context.model, order_by['field'])  # @todo context.model vs. context.entity.__class__ ?
+      order_by_field = getattr(model, order_by['field'])
       asc = order_by['operator'] == 'asc'
       if asc:
         query = query.order(order_by_field)
       else:
         query = query.order(-order_by_field)
-    if self.search_filter_callback:
-      query = self.search_filter_callback(context, query)  # @todo Not sure how to deal with this in plugin?
     cursor = Cursor(urlsafe=context.input.get('next_cursor'))
     entities, next_cursor, more = query.fetch_page(settings.SEARCH_PAGE, start_cursor=cursor)
     if next_cursor:
       next_cursor = next_cursor.urlsafe()
-    # @todo Not sure how to deal with remaining lines in this in plugin? This run() has to be broken down for compliancy!
-    if self.search_entities_callback:
-      entities = self.search_entities_callback(context, entities)
-    for entity in entities:
-      context.rule.entity = entity
-      rule.Engine.run(context)
-      rule.read(entity)
-    context.output['entities'] = entities
+    # @todo Perfectly all of the output should be handeled on one place. Output lines below should be removed when output is optimized.
     context.output['next_cursor'] = next_cursor
     context.output['more'] = more
