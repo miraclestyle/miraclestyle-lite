39903aa52914d3aefb9de21b11fe3847ce06d60d
miraclestyle <elvinkosova@gmail.com>
2014-04-24 16:58:34 +0000

    callback.py edited online with Bitbucket

--- app/plugins/callback.py
+++ app/plugins/callback.py
@@ -11,83 +11,46 @@ from google.appengine.api import taskqueue
 
 from app import ndb
 from app.srv import event
-
-def prepare_attr(entity, field_path):
-  fields = field_path.split('.')
-  last_field = fields[-1]
-  drill = fields[:-1]
-  i = -1
-  while not last_field:
-    i = i - 1
-    last_field = fields[i]
-    drill = fields[:i]
-  for field in drill:
-    if field:
-      if isinstance(entity, dict):
-        try:
-          entity = entity[field]
-        except KeyError as e:
-          return None
-      elif isinstance(entity, list):
-        try:
-          entity = entity[int(field)]
-        except KeyError as e:
-          return None
-      else:
-        try:
-          entity = getattr(entity, field)
-        except ValueError as e:
-          return None
-  return (entity, last_field)
-
-def set_attr(entity, field_path, value):
-  entity, last_field = prepare_attr(entity, field_path)
-  if isinstance(entity, dict):
-    entity[last_field] = value
-  elif isinstance(entity, list):
-    entity.insert(int(last_field), value)
-  else:
-    setattr(entity, last_field, value)
-
-def get_attr(entity, field_path):
-  entity, last_field = prepare_attr(entity, field_path)
-  if isinstance(entity, dict):
-    return entity.get(last_field)
-  elif isinstance(entity, list):
-    return entity[int(last_field)]
-  else:
-    return getattr(entity, last_field)
-
-def execute(context, payloads, transactional=None):
-  queues = {}
-  url = '/task/io_engine_run'
-  if not isinstance(payloads, (list, tuple)):
-    payloads = [payloads]
-  if transactional is None:
-    transactional = ndb.in_transaction()
-  if transactional:
-    payloads = payloads[:5]
-  if len(payloads):
-    for payload in payloads:
-      data = {}
-      data.update(payload.internal_fields)
-      for key, value in payload.external_fields.items():
-        data[key] = get_attr(context, value)
-      if payload.queue not in queues:
-        queues[payload.queue] = []
-      queues[payload.queue].append(taskqueue.Task(url=url, payload=json.dumps(data)))
-  if len(queues):
-    for queue_name, tasks in queues.items():
-      queue = taskqueue.Queue(name=queue_name)
-      queue.add(tasks, transactional=transactional)
+from app.lib.attribute_manipulator import set_attr, get_attr
 
 
 class Payload(event.Plugin):
   
-  transactional = ndb.SuperBooleanProperty('4', indexed=False, required=True, default=True)
   queue = ndb.SuperStringProperty('5', indexed=False, required=True)
-  internal_fields = ndb.SuperJsonProperty('6', indexed=False, required=False, default={})  # @todo Not sure if this is the best way to store repeated key/value data pairs?
-  external_fields = ndb.SuperJsonProperty('7', indexed=False, required=False, default={})  # @todo Not sure if this is the best way to store repeated key/value data pairs?
+  payload_data = ndb.SuperJsonProperty('6', indexed=False, required=True, default={})
   
   def run(self, context):
-    execute(context, self, self.transactional)
+    data = {}
+    for key, value in self.payload_data.items():
+      data[key] = get_attr(context, value)
+    context.callback_payloads.append((self.queue, data))
+
+
+class Exec(event.Plugin):
+  
+  static_data = ndb.SuperJsonProperty('5', indexed=False, required=True, default={})
+  dynamic_data = ndb.SuperJsonProperty('6', indexed=False, required=True,
+                                       default={'caller_user': 'context.user.key_urlsafe',
+                                                'caller_action': 'context.action.key_urlsafe'})
+  
+  def run(self, context):
+    queues = {}
+    url = '/task/io_engine_run'
+    if self.transactional:
+      context.callback_payloads = context.callback_payloads[:5]
+    if len(context.callback_payloads):
+      for payload in context.callback_payloads:
+        data = {}
+        queue_name, payload_data = payload
+        data.update(self.static_data)
+        for key, value in self.dynamic_data.items():
+          data[key] = get_attr(context, value)
+        data.update(payload_data)
+        if queue_name not in queues:
+          queues[queue_name] = []
+        queues[queue_name].append(taskqueue.Task(url=url, payload=json.dumps(data)))
+    if len(queues):
+      for queue_name, tasks in queues.items():
+        queue = taskqueue.Queue(name=queue_name)
+        queue.add(tasks, transactional=self.transactional)
+    context.callback_payloads = []
