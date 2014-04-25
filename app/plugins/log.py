e1973788983748b5fd7a2e9fa4e8c4b87239a13b
miraclestyle <elvinkosova@gmail.com>
2014-04-25 09:05:29 +0000

    log.py edited online with Bitbucket

--- app/plugins/log.py
+++ app/plugins/log.py
@@ -13,6 +13,66 @@ from app.lib.attribute_manipulator import set_attr, get_attr
 from app.srv.log import Record
 
 
+def get_records(entity, urlsafe_cursor):
+  items_per_page = settings.RECORDS_PAGE
+  cursor = Cursor(urlsafe=urlsafe_cursor)
+  query = Record.query(ancestor=entity.key).order(-Record.logged)
+  entities, next_cursor, more = query.fetch_page(items_per_page, start_cursor=cursor)
+  if next_cursor:
+    next_cursor = next_cursor.urlsafe()
+  
+  @ndb.tasklet
+  def async(entity):
+    if entity.key_namespace:
+      domain_user_key = ndb.Key('8', str(entity.agent.id()), namespace=entity.key_namespace)
+      agent = yield domain_user_key.get_async()
+      agent = agent.name
+    else:
+      agent = yield entity.agent.get_async()
+      agent = agent._primary_email
+    entity._agent = agent
+    action_key_id = str(entity.action.id()).split('-')
+    if len(action_key_id) == 2:
+      kind_id, action_id = action_key_id
+      modelclass = entity._kind_map.get(kind_id)
+      if modelclass and hasattr(modelclass, '_actions'):
+        for action_key, action in modelclass._actions.items():
+          if entity.action == action.key:
+            entity._action = '%s.%s' % (modelclass.__name__, action_key)
+            break
+    else:
+      entity._action = entity.action.id()
+    raise ndb.Return(entity)
+  
+  @ndb.tasklet
+  def helper(entities):
+    results = yield map(async, entities)
+    raise ndb.Return(results)
+  
+  entities = helper(entities)
+  entities = [entity for entity in entities.get_result()]
+  return entities, next_cursor, more
+
+
+class Read(event.Plugin):
+  
+  kind_id = ndb.SuperStringProperty('5', indexed=False)
+  
+  def run(self, context):
+    if len(context.entities):
+      if isinstance(context.entities, dict):
+        if self.kind_id != None:
+          entities, next_cursor, more = get_records(context.entities[kind_id], context.input.get('next_cursor'))
+          context.entities[kind_id]._records = entities
+          context.next_cursor = next_cursor
+          context.more = more
+        else:
+          entities, next_cursor, more = get_records(context.entities[context.model.get_kind()], context.input.get('next_cursor'))
+          context.entities[context.model.get_kind()]._records = entities
+          context.next_cursor = next_cursor
+          context.more = more
+
+
 class Entity(event.Plugin):
   
   log_entities = ndb.SuperStringProperty('5', indexed=False, repeated=True)
