f62a41502efff2adcd3246cd6be238943d3d61d2
miraclestyle <elvinkosova@gmail.com>
2014-04-28 17:03:48 +0000

    notify.py edited online with Bitbucket

--- app/plugins/notify.py
+++ app/plugins/notify.py
@@ -12,26 +12,9 @@ from app.srv import event
 from app.lib.attribute_manipulator import set_attr, get_attr
 
 
-def set_context(context):
-  if not hasattr(context, 'entities'):
-    context.entities = {}
-  if not hasattr(context, 'values'):
-    context.values = {}
-  # @todo Following lines are temporary!
-  context.user = context.auth.user
-  domain_key = context.input.get('domain')
-  if domain_key:
-    context.domain = domain_key.get()
-  if not hasattr(context, 'callback_payloads'):
-    context.callback_payloads = []
-  if not hasattr(context, 'log_entities'):
-    context.log_entities = []
-
-
 class Prepare(event.Plugin):
   
   def run(self, context):
-    set_context(context)
     caller_entity_key = context.input.get('caller_entity')
     context.caller_entity = caller_entity_key.get()  # @todo If user is taskqueue (as is expected to be) how do we handle it in rule?
     context.entities[context.model.get_kind()] = context.model(namespace=context.caller_entity.key_namespace)
