4e4e775e457efc209857cc04628a2dcd9a1f6e73
miraclestyle <elvinkosova@gmail.com>
2014-05-19 12:54:40 +0000

    setup.py draft!

--- app/srv/setup.py
+++ app/srv/setup.py
@@ -27,8 +27,8 @@ class Configuration(ndb.BaseExpando):
   
   _global_role = GlobalRole(
     permissions=[
-      ActionPermission('57', [Action.build_key('57', 'install').urlsafe(),
-                              Action.build_key('57', 'cron_install').urlsafe()], True, 'context.user._is_taskqueue')
+      ActionPermission('57', [Action.build_key('57', 'install'),
+                              Action.build_key('57', 'cron_install')], True, 'context.user._is_taskqueue')
       ]
     )
   
