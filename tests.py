2b6cd4ed6eefab74e741d0c0b0b73b455329da07
admin <vertazzar@gmail.com>
2014-07-08 19:50:01 +0200

    better testing environment

--- tests.py
+++ tests.py
@@ -9,6 +9,33 @@ import random
 
 # example of testcase
 
+class Base1(object):
+  
+  def ove(self):
+    print '1'
+    super(Base1, self).ove()
+  
+  def foo(self):
+    print 'bar'
+    
+    
+class Base2(object):
+  
+  def bar(self):
+    self.foo()
+    
+  def ove(self):
+    print '2'
+    
+class Baz(Base1, Base2):
+  
+  def write(self):
+    self.bar()
+    self.ove()
+    
+ff = Baz()
+ff.write()
+
 class TestSequenceFunctions(unittest.TestCase):
 
     def setUp(self):
