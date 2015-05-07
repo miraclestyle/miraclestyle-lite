fb1bbeee1e1094d288b10c853c55635b08607f2e
admin <vertazzar@gmail.com>
2015-05-07 21:48:07 +0200

    snackbar

---
+++ frontend/client/src/core/snackbar/snackbar.js
@@ -0,0 +1,86 @@
+(function () {
+    'use strict';
+    angular.module('app').factory('snackbar', ['GLOBAL_CONFIG', function (GLOBAL_CONFIG) {
+        var snackbar = {
+            show: $.noop,
+            hide: $.noop
+        };
+        if (GLOBAL_CONFIG.debug) {
+            window._snackbar = snackbar;
+        }
+        return snackbar;
+    }]).directive('qsnackbar', function (snackbar) {
+        return {
+            link: function (scope, element) {
+                var kill = function () {
+                    snackbar.hide();
+                };
+                element.on('click', kill);
+                scope.$on('$destroy', function () {
+                    element.off('click', kill);
+                });
+            }
+        };
+    }).directive('snackbar', function (snackbar, $timeout, $animate, $q) {
+        return {
+            scope: true,
+            require: 'snackbar',
+            templateUrl: 'core/snackbar/view.html',
+            controller: function ($scope) {
+                var digest = function () {
+                    if (!$scope.$$phase) {
+                        $scope.$digest();
+                    }
+                }, timer;
+                $scope.message = '';
+                $scope.size = 1;
+                $scope.element = null;
+                snackbar.hide = function () {
+                    var defer = $q.defer();
+                    $scope.element.removeClass('in');
+                    if (!$scope.element.hasClass('out')) {
+                        $animate.addClass($scope.element, 'out').then(function () {
+                            defer.resolve();
+                        });
+                        digest();
+                    } else {
+                        defer.resolve();
+                    }
+                    return defer.promise;
+                };
+                snackbar.show = function (config) {
+                    if (!angular.isObject(config)) {
+                        config = {
+                            message: config
+                        };
+                    }
+                    $scope.message = config.message;
+                    if (!config.size) {
+                        config.size = 1;
+                    }
+                    if (!config.hideAfter) {
+                        config.hideAfter = (($scope.message.length / 16) * 1000) + 500;
+                    }
+                    $scope.size = config.size;
+                    digest();
+                    return snackbar.hide().then(function () {
+                        $animate.removeClass($scope.element, 'out');
+                        return $animate.addClass($scope.element, 'in').then(function () {
+                            if (config.hideAfter) {
+                                if (timer) {
+                                    clearTimeout(timer);
+                                }
+                                timer = setTimeout(function () {
+                                    snackbar.hide();
+                                }, config.hideAfter);
+                            }
+                        });
+                    });
+                };
+            },
+            link: function (scope, element, snackbarCtrl) {
+                scope.element = element.find('.snackbar');
+            }
+        };
+    });
+}());
--- tests.py
+++ tests.py
@@ -1,18 +1,78 @@
-def function(message=None):
-    def decorator(fn):
-        def inner(*args, **kwargs):
-            result = fn(*args, **kwargs)
-            result.append(message)
-            return result
-        return inner
-    return decorator
+import timeit
+import sys
+import time
+
+sys.setrecursionlimit(200000)
+
+def recurse():
+    recurse()
+
+recurse()
+
+STACK = 10000
+
+class Increaser:
+
+    def __init__(self):
+        self.n = 0
+
+    def incr(self):
+        self.n += 1
+        return self.n
+
+n1 = Increaser()
+n2 = Increaser()
+
+def test_recursion2():
+    while True:
+        try:
+            if n1.incr() == STACK:
+                raise Exception('stop')
+        except:
+            break
+
+def test_recursion():
+    try:
+        if n2.incr() == STACK:
+            raise Exception('stop')
+        test_recursion()
+    except:
+        pass
+
+print 'Test recursion'
+start = time.time()
+test_recursion()
+print 'in %sms' % ((time.time() - start) * 1000)
+
+print 'Test While'
+start = time.time()
+test_recursion2()
+print 'in %sms' % ((time.time() - start) * 1000)
 
 
-@function(message='Yes')
-def go(*args, **kwargs):
-    return [args, kwargs]
+setup = """
+class Increaser:
 
-print go(1,2,3,4,5, foo=1)
-print go.__name__
-nem = '%s what %s'
-print nem
+    def __init__(self):
+        self.n = 0
+
+    def incr(self):
+        self.n += 1
+        return self.n
+
+n1 = Increaser()
+n2 = Increaser()
+
+def test_recursion2():
+    while True:
+        try:
+            if n1.incr() == STACK:
+                raise Exception('stop')
+        except:
+            break
+
+"""
+
+print timeit.timeit('test_recursion2()', setup=setup, number=1)
+
+exit()
