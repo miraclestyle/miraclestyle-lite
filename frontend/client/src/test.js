d8868ad17c6b5d8327b84ef780adbb4596f18269
vertazzar <vertazzar@gmail.com>
2015-11-12 16:46:29 +0100

    work

--- frontend/client/src/test.js
+++ frontend/client/src/test.js
@@ -2,3 +2,19 @@ function a() {
     var thing = $('.order-stage:first');
     thing.removeClass('in').addClass('out').next().removeClass('out').addClass('in');
 }
+
+function slow(times) {
+    if (!times) {
+        console.log('done');
+        return;
+    }
+    var i = 100000;
+    while (i--) {
+        if (i === 1) {
+            setTimeout(function () {
+                times--;
+                slow(times);
+            }, 1);
+        }
+    }
+}
