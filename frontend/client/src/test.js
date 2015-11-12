cff09e160d9af1b4b938f85a0692225dcfd3f32f
vertazzar <vertazzar@gmail.com>
2015-11-12 18:05:28 +0100

    deploy

--- frontend/client/src/test.js
+++ frontend/client/src/test.js
@@ -3,18 +3,20 @@ function a() {
     thing.removeClass('in').addClass('out').next().removeClass('out').addClass('in');
 }
 
-function slow(times) {
-    if (!times) {
-        console.log('done');
-        return;
-    }
-    var i = 100000;
-    while (i--) {
-        if (i === 1) {
-            setTimeout(function () {
-                times--;
-                slow(times);
-            }, 1);
-        }
+function fib(x) {
+    if (x === 0) {
+        return 0;
+    } else if (x === 1) {
+        return 1;
+    } else {
+        return fib(x - 1) + fib(x - 2);
     }
 }
+
+var cle = setInterval(function () {
+    fib(40);
+}, 100);
+
+var stop = function () {
+    clearInterval(cle);
+};
