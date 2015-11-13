c7164af36a60354d459e26a603cffb21f847dda6
vertazzar <vertazzar@gmail.com>
2015-11-13 12:38:35 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,12 @@ git add -A
 git commit -m"deploy"
 git push
 appcfg.py update backend/app.yaml
-sleep 15
+sleep 5
+echo "Waiting for frontend deployment... 5s"
+sleep 5
+echo "Waiting for frontend deployment... 10s"
+sleep 5
+echo "Waiting for frontend deployment... 15s"
 appcfg.py update frontend/app.yaml
 appcfg.py update_dispatch .
 appcfg.py update_indexes backend
