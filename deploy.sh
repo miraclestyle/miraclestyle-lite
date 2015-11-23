7352f105650c37bd5855f01215a966b9987ffeef
vertazzar <vertazzar@gmail.com>
2015-11-23 20:13:10 +0100

    deploy all

--- deploy.sh
+++ deploy.sh
@@ -2,40 +2,33 @@
 #
 git pull
 
-if [ "$1" = "frontend" ] || ["$1" = "all"]
-then
+if [ "$1" = "frontend" ] || ["$1" = "all"]; then
 sh build.sh
 fi
 
 git add -A
-if [ "$1" = "frontend" ]
-then
+if [ "$1" = "frontend" ]; then
     git commit -m"deploy frontend"
-elif [ "$1" = "backend"]
-    then
+elif [ "$1" = "backend" ]; then
     git commit -m"deploy backend"
-elif ["$1" = "all"]
-    then
+elif [ "$1" = "all" ]; then
     git commit -m"deploy all"
 fi
 git push
 
-if [ "$1" = "backend" ] || ["$1" = "all"]
-then
+if [ "$1" = "backend" ] || [ "$1" = "all" ]; then
     appcfg.py update backend/app.yaml
-else
-    echo "Just updating backend..."
-fi
-
-if [ "$1" = "frontend" ] || ["$1" = "all"]
-then
-    appcfg.py update frontend/app.yaml
 else
     echo "Just updating frontend..."
 fi
 
-if [ "$2" = "index" ]
-then
+if [ "$1" = "frontend" ] || [ "$1" = "all" ]; then
+    appcfg.py update frontend/app.yaml
+else
+    echo "Just updating backend..."
+fi
+
+if [ "$2" = "index" ]; then
     appcfg.py update_dispatch .
     appcfg.py update_indexes backend
 fi
