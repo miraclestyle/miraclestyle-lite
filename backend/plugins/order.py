d9718701512174e270190382f66c281e78991c77
miraclestyle <elvinkosova@gmail.com>
2015-12-13 03:06:11 +0000

    order.py edited online with Bitbucket

--- backend/plugins/order.py
+++ backend/plugins/order.py
@@ -518,6 +518,7 @@ class OrderNotify(orm.BaseModel):
   _kind = 118
 
   def run(self, context):
+    tools.log.debug('Start OrderNotify execution!')
     order = getattr(self, 'find_order_%s' % context.input.get('payment_method'))(context)  # will throw an error if the payment method does not exist
     context._order = order
     order.read({'_lines': {'config': {'search': {'options': {'limit': 0}}}}, '_payment_method': {}})
