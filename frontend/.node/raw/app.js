35d72366b9dc5d7516144a24f3534267493a9b16
vertazzar <vertazzar@gmail.com>
2015-12-30 14:33:44 +0100

    fix - build

--- frontend/.node/raw/app.js
+++ frontend/.node/raw/app.js
@@ -5155,6 +5155,7 @@ function msieversion() {
 
                 var ignore = element.attr('md-ink-ripple-ignore'),
                     eventHandler = (!element.attr('md-ink-ripple-click') ? '$md.pressdown' : 'click');
+                    eventHandler = 'click';
 
                 ignore = (ignore ? $parse(ignore)(scope) : undefined);
 
