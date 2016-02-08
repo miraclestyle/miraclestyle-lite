422342c5076b7877902ae78e069b036100143423
vertazzar <vertazzar@gmail.com>
2016-02-08 23:13:02 +0100

    flooring float must be done - deploy all

--- frontend/client/src/core/slider/slider.js
+++ frontend/client/src/core/slider/slider.js
@@ -100,7 +100,7 @@
                         var bar = element.parents('.modal:first').find('.new-pricetag-bar'),
                             wholeHeight = element.parents('.fixed-height:first').innerHeight() - window.SCROLLBAR_WIDTH - (bar.length ? bar.outerHeight() : 0),
                             newHeight = wholeHeight,
-                            newWidth = (newHeight * image.proportion),
+                            newWidth = Math.round(newHeight * image.proportion),
                             imageSize = newHeight > newWidth ? newHeight : newWidth,
                             originalNewHeight = newHeight,
                             reactingElement = element.parents('.image-slider-item:first'),
